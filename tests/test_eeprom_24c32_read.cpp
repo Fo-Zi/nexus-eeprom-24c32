ed33fa129e3ee5aa35960ddeabd7159fe09e7d06
Fo-Zi <facundozingarelli@gmail.com>
2025-08-26 11:29:26 -0300

    Adding missing tests and updating gitignore

---
+++ tests/test_eeprom_24c32_read.cpp
@@ -0,0 +1,110 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "nhal_i2c_mock.hpp"
+
+extern "C" {
+    #include "eeprom_24c32.h"
+}
+
+using ::testing::_;
+using ::testing::Return;
+using ::testing::SetArrayArgument;
+
+class Eeprom24c32ReadTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        memset(&handle, 0, sizeof(handle));
+        memset(&ctx, 0, sizeof(ctx));
+        
+        handle.ctx = &ctx;
+        handle.device_address = 0x50;
+        handle.timeout_ms = 1000;
+        
+        testing::Mock::VerifyAndClearExpectations(&NhalI2cMock::instance());
+    }
+
+    void TearDown() override {
+        testing::Mock::VerifyAndClearExpectations(&NhalI2cMock::instance());
+    }
+
+    eeprom_24c32_handle_t handle;
+    struct nhal_i2c_context ctx;
+};
+
+TEST_F(Eeprom24c32ReadTest, ReadValidData) {
+    uint8_t expected_data[] = {0xAA, 0xBB, 0xCC, 0xDD};
+    uint8_t read_buffer[4];
+    uint16_t address = 0x0100;
+
+    EXPECT_CALL(NhalI2cMock::instance(), nhal_i2c_master_write_read_reg(
+        &ctx, 0x50, _, 2, read_buffer, 4, 1000))
+        .WillOnce([&](struct nhal_i2c_context* ctx, nhal_i2c_slave_addr addr, const uint8_t* reg, size_t reg_len, uint8_t* data, size_t data_len, nhal_timeout_ms timeout) {
+            memcpy(data, expected_data, 4);
+            return NHAL_OK;
+        });
+
+    eeprom_24c32_result_t result = eeprom_24c32_read(&handle, address, read_buffer, 4);
+
+    EXPECT_EQ(result, EEPROM_24C32_OK);
+    EXPECT_EQ(memcmp(read_buffer, expected_data, 4), 0);
+}
+
+TEST_F(Eeprom24c32ReadTest, ReadNullHandle) {
+    uint8_t buffer[4];
+    
+    eeprom_24c32_result_t result = eeprom_24c32_read(nullptr, 0, buffer, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32ReadTest, ReadNullBuffer) {
+    eeprom_24c32_result_t result = eeprom_24c32_read(&handle, 0, nullptr, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32ReadTest, ReadZeroLength) {
+    uint8_t buffer[4];
+    
+    eeprom_24c32_result_t result = eeprom_24c32_read(&handle, 0, buffer, 0);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32ReadTest, ReadAddressOutOfRange) {
+    uint8_t buffer[4];
+    
+    eeprom_24c32_result_t result = eeprom_24c32_read(&handle, EEPROM_24C32_SIZE_BYTES, buffer, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_ADDRESS_OUT_OF_RANGE);
+}
+
+TEST_F(Eeprom24c32ReadTest, ReadLengthExceedsSize) {
+    uint8_t buffer[4];
+    
+    eeprom_24c32_result_t result = eeprom_24c32_read(&handle, EEPROM_24C32_SIZE_BYTES - 2, buffer, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_ADDRESS_OUT_OF_RANGE);
+}
+
+TEST_F(Eeprom24c32ReadTest, ReadI2CError) {
+    uint8_t buffer[4];
+    
+    EXPECT_CALL(NhalI2cMock::instance(), nhal_i2c_master_write_read_reg(_, _, _, _, _, _, _))
+        .WillOnce(Return(NHAL_ERR_OTHER));
+
+    eeprom_24c32_result_t result = eeprom_24c32_read(&handle, 0, buffer, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_I2C_ERROR);
+}
+
+TEST_F(Eeprom24c32ReadTest, ReadTimeout) {
+    uint8_t buffer[4];
+    
+    EXPECT_CALL(NhalI2cMock::instance(), nhal_i2c_master_write_read_reg(_, _, _, _, _, _, _))
+        .WillOnce(Return(NHAL_ERR_TIMEOUT));
+
+    eeprom_24c32_result_t result = eeprom_24c32_read(&handle, 0, buffer, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_WRITE_TIMEOUT);
+}
---
+++ tests/test_eeprom_24c32_write.cpp
@@ -0,0 +1,152 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "nhal_i2c_mock.hpp"
+
+extern "C" {
+    #include "eeprom_24c32.h"
+}
+
+using ::testing::_;
+using ::testing::Return;
+using ::testing::InSequence;
+
+class Eeprom24c32WriteTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        memset(&handle, 0, sizeof(handle));
+        memset(&ctx, 0, sizeof(ctx));
+        
+        handle.ctx = &ctx;
+        handle.device_address = 0x50;
+        handle.timeout_ms = 1000;
+        
+        testing::Mock::VerifyAndClearExpectations(&NhalI2cMock::instance());
+    }
+
+    void TearDown() override {
+        testing::Mock::VerifyAndClearExpectations(&NhalI2cMock::instance());
+    }
+
+    eeprom_24c32_handle_t handle;
+    struct nhal_i2c_context ctx;
+};
+
+TEST_F(Eeprom24c32WriteTest, WritePageSuccess) {
+    uint8_t data[] = {0x01, 0x02, 0x03, 0x04};
+    
+    EXPECT_CALL(NhalI2cMock::instance(), nhal_i2c_master_write(&ctx, 0x50, _, 6, 1000))
+        .WillOnce(Return(NHAL_OK));
+
+    eeprom_24c32_result_t result = eeprom_24c32_write_page(&handle, 0, data, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_OK);
+}
+
+TEST_F(Eeprom24c32WriteTest, WritePageNullHandle) {
+    uint8_t data[] = {0x01, 0x02, 0x03, 0x04};
+    
+    eeprom_24c32_result_t result = eeprom_24c32_write_page(nullptr, 0, data, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32WriteTest, WritePageNullData) {
+    eeprom_24c32_result_t result = eeprom_24c32_write_page(&handle, 0, nullptr, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32WriteTest, WritePageZeroLength) {
+    uint8_t data[] = {0x01, 0x02, 0x03, 0x04};
+    
+    eeprom_24c32_result_t result = eeprom_24c32_write_page(&handle, 0, data, 0);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32WriteTest, WritePageTooLarge) {
+    uint8_t data[EEPROM_24C32_PAGE_SIZE_BYTES + 1];
+    
+    eeprom_24c32_result_t result = eeprom_24c32_write_page(&handle, 0, data, sizeof(data));
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32WriteTest, WritePageCrossesBoundary) {
+    uint8_t data[16];
+    uint16_t address = EEPROM_24C32_PAGE_SIZE_BYTES - 8; // 8 bytes from page end
+    
+    eeprom_24c32_result_t result = eeprom_24c32_write_page(&handle, address, data, 16);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32WriteTest, WritePageAddressOutOfRange) {
+    uint8_t data[] = {0x01, 0x02, 0x03, 0x04};
+    
+    eeprom_24c32_result_t result = eeprom_24c32_write_page(&handle, EEPROM_24C32_SIZE_BYTES, data, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_ADDRESS_OUT_OF_RANGE);
+}
+
+TEST_F(Eeprom24c32WriteTest, WriteSuccess) {
+    uint8_t data[] = {0x01, 0x02, 0x03, 0x04};
+    
+    // Expect write operation
+    EXPECT_CALL(NhalI2cMock::instance(), nhal_i2c_master_write(_, _, _, _, _))
+        .WillOnce(Return(NHAL_OK));
+        
+    // Expect ready check (polling for write completion)
+    EXPECT_CALL(NhalI2cMock::instance(), nhal_i2c_master_read(_, _, _, _, 100))
+        .WillOnce(Return(NHAL_OK)); // Ready immediately
+
+    eeprom_24c32_result_t result = eeprom_24c32_write(&handle, 0, data, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_OK);
+}
+
+TEST_F(Eeprom24c32WriteTest, WriteNullHandle) {
+    uint8_t data[] = {0x01, 0x02, 0x03, 0x04};
+    
+    eeprom_24c32_result_t result = eeprom_24c32_write(nullptr, 0, data, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32WriteTest, WriteNullData) {
+    eeprom_24c32_result_t result = eeprom_24c32_write(&handle, 0, nullptr, 4);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32WriteTest, WriteZeroLength) {
+    uint8_t data[] = {0x01, 0x02, 0x03, 0x04};
+    
+    eeprom_24c32_result_t result = eeprom_24c32_write(&handle, 0, data, 0);
+    
+    EXPECT_EQ(result, EEPROM_24C32_ERR_INVALID_ARG);
+}
+
+TEST_F(Eeprom24c32WriteTest, IsReadyTrue) {
+    EXPECT_CALL(NhalI2cMock::instance(), nhal_i2c_master_read(&ctx, 0x50, _, 1, 100))
+        .WillOnce(Return(NHAL_OK));
+
+    bool ready = eeprom_24c32_is_ready(&handle);
+    
+    EXPECT_TRUE(ready);
+}
+
+TEST_F(Eeprom24c32WriteTest, IsReadyFalse) {
+    EXPECT_CALL(NhalI2cMock::instance(), nhal_i2c_master_read(&ctx, 0x50, _, 1, 100))
+        .WillOnce(Return(NHAL_ERR_OTHER));
+
+    bool ready = eeprom_24c32_is_ready(&handle);
+    
+    EXPECT_FALSE(ready);
+}
+
+TEST_F(Eeprom24c32WriteTest, IsReadyNullHandle) {
+    bool ready = eeprom_24c32_is_ready(nullptr);
+    
+    EXPECT_FALSE(ready);
+}
